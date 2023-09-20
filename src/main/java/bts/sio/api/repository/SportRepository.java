997314c9113947ab70a13c4c1a8cd7efeded937b
HRVNoa <hervieu.noa@gmail.com>
2023-09-20 09:36:00 +0200

    Creation des fonction CRUD de SportService

---
+++ src/main/java/bts/sio/api/repository/SportRepository.java
@@ -0,0 +1,9 @@
+package bts.sio.api.repository;
+
+import bts.sio.api.model.Sport;
+import org.springframework.data.repository.CrudRepository;
+
+public interface SportRepository extends CrudRepository<Sport, Long> {
+
+
+}
